2e90777e3c4b702ea33087b3ff6855e75a449e76
Uncle Pedro <peterforsyth10@gmail.com>
2024-10-16 11:45:47 +1000

    Create newUser function - Generates random username and apiKey - Create route for frontend to access

--- api/index.ts
+++ api/index.ts
@@ -12,6 +12,38 @@ app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cors(corsOptions)); // Apply CORS middleware with the specified options to the Express app
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+import crypto from "crypto";
+
+// async function loadInspirationalQuotes() {
+//   for (const { quote, author } of inspirationalQuotes) {
+//     await addQuote(quote, author, "", false); // Set isUserQuote to false for predefined quotes
+//   }
+//   console.log("Inspirational quotes loaded successfully!");
+// }
+
+const getRandomQuote = async () => {
+  const allQuotes = await prisma.quote.findMany();
+  const randomIndex = Math.floor(Math.random() * allQuotes.length);
+  return allQuotes[randomIndex];
+};
+
+// Define a route handler for GET requests made to the /randomquote endpoint
+app.get("/randomQuote", async (req: Request, res: Response) => {
+  res.json(await getRandomQuote());
+});
+
+async function getUserQuotes() {
+  return await prisma.quote.findMany({
+    where: {
+      isUserQuote: true,
+    },
+  });
+}
+
+// Get new quotes user has added
+app.get("/getUserQuotes", async (req: Request, res: Response) => {
+  res.json(await getUserQuotes());
+});
 
 async function addQuote(
   quote: string,
@@ -29,45 +61,6 @@ async function addQuote(
   });
 }
 
-// async function loadInspirationalQuotes() {
-//   for (const { quote, author } of inspirationalQuotes) {
-//     await addQuote(quote, author, false); // Set isUserQuote to false for predefined quotes
-//   }
-//   console.log("Inspirational quotes loaded successfully!");
-// }
-
-async function deleteQuote(quoteId: number) {
-  await prisma.quote.delete({
-    where: {
-      quoteId: quoteId,
-    },
-  });
-}
-
-async function getUserQuotes() {
-  return await prisma.quote.findMany({
-    where: {
-      isUserQuote: true,
-    },
-  });
-}
-
-const getRandomQuote = async () => {
-  const allQuotes = await prisma.quote.findMany();
-  const randomIndex = Math.floor(Math.random() * allQuotes.length);
-  return allQuotes[randomIndex];
-};
-
-// Define a route handler for GET requests made to the /randomquote endpoint
-app.get("/randomQuote", async (req: Request, res: Response) => {
-  res.json(await getRandomQuote());
-});
-
-// Get new quotes user has added
-app.get("/getUserQuotes", async (req: Request, res: Response) => {
-  res.json(await getUserQuotes());
-});
-
 // Add new quote to database
 app.post("/addQuote", async (req: Request, res: Response) => {
   addQuote(req.body.quote, req.body.author, req.body.userId);
@@ -78,6 +71,14 @@ app.post("/addQuote", async (req: Request, res: Response) => {
     .status(201)
     .json({ message: "Quote added successfully", quotes: updatedUserQuotes });
 });
+
+async function deleteQuote(quoteId: number) {
+  await prisma.quote.delete({
+    where: {
+      quoteId: quoteId,
+    },
+  });
+}
 
 // Delete quote from database
 app.delete("/deleteQuote", async (req: Request, res: Response) => {
@@ -92,6 +93,40 @@ app.delete("/deleteQuote", async (req: Request, res: Response) => {
     message: "Quote deleted successfully",
     quotes: updatedUserQuotes, // Send the updated quotes back to the client
   });
+});
+
+interface User {
+  username: string;
+  apiKey: string;
+}
+
+// Function to create a new user with random username and API key
+const newUser = async () => {
+  const username = crypto.randomBytes(8).toString("hex");
+  const apiKey = crypto.randomBytes(32).toString("hex");
+
+  // Store the user with the username and API key in the database
+  const newUser: User = await prisma.user.create({
+    data: {
+      username,
+      apiKey,
+    },
+  });
+
+  // Return the API key (and username, if needed) to the caller
+  return newUser;
+};
+
+app.post("/generateUser", async (req: Request, res: Response) => {
+  try {
+    const newUserData: User = await newUser(); // Create a new user with an API key
+    res.status(201).json({
+      message: "User created successfully",
+      newUserData: newUserData,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create user" });
+  }
 });
 
 app.listen(8080, () => {
---
+++ prisma/migrations/20241016014347_new_user/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "username" TEXT NOT NULL,
+    "apiKey" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("apiKey")
+);
